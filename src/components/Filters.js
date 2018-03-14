60b963d399747b853fbd64563dfe7b3517b8f3f6
GuttermanA <alexanderf.gutterman@gmail.com>
2018-03-14 11:52:43 -0400

    still working

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,12 +5,20 @@ class Filters extends React.Component {
     super();
   }
 
+  handleFilterTypeChange = (event) => {
+    this.props.onChangeType(event.target.selectedOptions[0].value);
+  }
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select  name="type"
+            id="type"
+            value={this.props.filters.type}
+            onChange={this.handleFilterTypeChange}
+            >
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -19,7 +27,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
